18265e32d99995b23bc72793c89e1095bd479810
Developerindia2026 <developmentindia2026@gmail.com>
2026-08-02 19:43:26 -0500

    fix api

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,9 +1,7 @@
-const API = "http://localhost:3000";
-
+const API = "https://quickcart-main-vy3p.onrender.com";
 
 export default API;
 
-
-// https://quickcart-main-vy3p.onrender.com"  
+// https://quickcart-main-vy3p.onrender.com"
 
 // http://localhost:3000
